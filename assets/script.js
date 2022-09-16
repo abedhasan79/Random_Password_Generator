2b8491bc94c3f21fe6aba0cbd52c1eb0bbe1d29f
Abed Hasan <abedhasan79@gmail.com>
2022-09-16 11:02:54 -0400

    added comment in javascript

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,73 @@
 // Assignment Code
 let criteria="Password Criteria:"+"\n"+"1. Password mush be between 8 to 128 characters long." + "\n" +
-"2. Password can include lowercase, uppercase, number and/or special characters.";
+"2. Password should include lowercase, uppercase, number and/or special characters.";
 
 let lengthCriteria = "Choose a length of at least 8 to 128 characters";
 
-let passCriteria;
 
+let lowercase="abcdefghijklmnopqrstuvwxyz"; // lowercase characters
 
-let lowercase="abcdefghijklmnopqrstuvwxyz";
+let uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //uppercase characters
 
-let uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numeric="0123456789" // numeric characters
 
-let numeric="0123456789"
-
-let specialCharacters=/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+let specialCharacters=/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/; //special characters
 
 let generateBtn = document.querySelector("#generate");
 function generatePassword() {
-  let finalCharacter="";
-  passCriteria = alert(criteria);
+
+  let finalCharacter=""; // empty string to store all the character type a user wants.
+
+  alert(criteria); //alerts the passward criteria
 
   let length = prompt(lengthCriteria)
+
+  //passward must be between 8 to  128 character.
   if (length < 8 || length > 128) {
     alert("Password length has to be between 8 to 128 characters");
     generatePassword();
   }
+
+  //if user wants lowercase character in their passward add lower case characters to the string finalCharacters
   if(window.confirm("Do you want to include lowercase character")){
     finalCharacter+=lowercase;
   }
+
+  //if user wants uppercase character in their passward add uppercase characters to the string finalCharacters
   if(window.confirm("Do you want to include uppercase character")){
     finalCharacter+=uppercase;
   }
+
+  //if user wants numeric characters in their passward add numeric characters to the string finalCharacters
   if(window.confirm("Do you want to include numeric character")){
     finalCharacter+=numeric;
   }
+
+  //if user wants special characters in their passward add special characters to the string finalCharacters
   if(window.confirm("Do you want to include special character")){
     finalCharacter+=specialCharacters;
   }
+
+  //user must choose one character type
   if(finalCharacter ===""){
     alert("must choose either uppercase or lowercase or numeric or special character");
     generatePassword();
   }
   // console.log(finalCharacter);
   
+  //creats a random passward based on the users choie of character types that got added in the string finalCharacter
   let randomPassword="";
   for(let i=0;i<length;i++){
     randomPassword = randomPassword + finalCharacter.charAt(Math.floor(Math.random() * finalCharacter.length));
   }
-  alert("Your password is: "+randomPassword);
-  return randomPassword;
 
+  //alerts the passward in a alert box
+  alert("Your password is: "+randomPassword);
+
+  return randomPassword;
 }
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
