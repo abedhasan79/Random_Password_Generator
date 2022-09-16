3e12562b97f5b15f41e27a60fa1fb8bb80f7879b
Abed Hasan <abedhasan79@gmail.com>
2022-09-15 21:13:26 -0400

    added code on javascript to get all prompts

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,55 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let criteria="Password Criteria:"+"\n"+"1. Password mush be between 8 to 128 characters long." + "\n" +
+"2. Password can include lowercase, uppercase, number and/or special characters.";
 
+let lengthCriteria = "Choose a length of at least 8 to 128 characters";
+
+let passCriteria;
+
+
+let lowercase="abcdefghijklmnopqrstuvwxyz";
+
+let uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let numeric="0123456789"
+
+let specialCharacters=/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+let generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  let finalCharacter="";
+  passCriteria = alert(criteria);
+
+  let length = prompt(lengthCriteria)
+  if (length < 8 || length > 128) {
+    alert("Password length has to be between 8 to 128 characters");
+    generatePassword();
+  }
+  if(window.confirm("Do you want to include lowercase character")){
+    finalCharacter+=lowercase;
+  }
+  if(window.confirm("Do you want to include uppercase character")){
+    finalCharacter+=uppercase;
+  }
+  if(window.confirm("Do you want to include numeric character")){
+    finalCharacter+=numeric;
+  }
+  if(window.confirm("Do you want to include special character")){
+    finalCharacter+=specialCharacters;
+  }
+  if(finalCharacter ===""){
+    alert("must choose either uppercase or lowercase or numeric or special character");
+    generatePassword();
+  }
+  // console.log(finalCharacter);
+  
+  let randomPassword="";
+  for(let i=0;i<length;i++){
+    randomPassword = randomPassword + finalCharacter.charAt(Math.floor(Math.random() * finalCharacter.length));
+  }
+  return randomPassword;
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
